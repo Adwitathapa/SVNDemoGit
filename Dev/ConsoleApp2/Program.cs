7ba177cbe0409d5a9abe42a9d60a8b44239bf2d9
rahul <rahul@65755985-ed21-8b49-8b9c-6a6564ddb146>
2024-08-07 08:25:05 +0000

    git-svn-id: https://duc-vmtomdfopoe.dedalus.lan/svn/Test/branches@5 65755985-ed21-8b49-8b9c-6a6564ddb146

---
+++ Dev/ConsoleApp2/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp2;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter username:");
+string userName = Console.ReadLine();
+Console.WriteLine("Username is: " + userName);
