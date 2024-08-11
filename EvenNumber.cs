b5accdd1184694ef658dd532884691085e64bd17
rahul <rahul@65755985-ed21-8b49-8b9c-6a6564ddb146>
2024-08-11 14:55:43 +0000

    Added new even class
    git-svn-id: https://duc-vmtomdfopoe.dedalus.lan/svn/Test/branches/Dev@8 65755985-ed21-8b49-8b9c-6a6564ddb146

---
+++ EvenNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class EvenNumber
+{
+    public static void main(String[] args)
+    {
+      
+        Console.Write("Find if given number is even");
+       Console.WriteLine("Enter the number: ");
+        int num1 = Console.ReadLine();
+        String number = (num1 % 2 == 0) ? "Even" : "Odd";
+
+        Console.Write("Entered number " + number);
+
+
+    }
+}
