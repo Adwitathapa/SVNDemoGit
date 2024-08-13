3973b6f5433ae67d3b8055696b05ab9e5c99d9c3
rahul <rahul@65755985-ed21-8b49-8b9c-6a6564ddb146>
2024-08-13 12:11:38 +0000

    Add one line
    git-svn-id: https://duc-vmtomdfopoe.dedalus.lan/svn/Test/branches/Dev@9 65755985-ed21-8b49-8b9c-6a6564ddb146

--- EvenNumber.cs
+++ EvenNumber.cs
@@ -11,6 +11,7 @@ public class EvenNumber
         String number = (num1 % 2 == 0) ? "Even" : "Odd";
 
         Console.Write("Entered number " + number);
+        Console.Write("Finished")
 
 
     }
